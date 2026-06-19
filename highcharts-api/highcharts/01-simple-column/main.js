57c2579e88742074c3c545dee5f8c5315423bf70
dqmrf <mykhailopelykh@tuta.io>
2026-06-19 13:48:29 +0200

    H01: Chart refactoring

--- highcharts-api/highcharts/01-simple-column/main.js
+++ highcharts-api/highcharts/01-simple-column/main.js
@@ -1,15 +1,11 @@
 const cityNames = ['Tokyo', 'New York', 'London'];
 
 const maxY = 9;
-let highestY = 0;
-
 const seriesDataLength = 3;
 
 const series = cityNames.map(city => {
     const cityData = Array.from({ length: seriesDataLength }, () => {
-        const value = Math.floor(Math.random() * (maxY + 1));
-        if (value > highestY) highestY = value;
-        return value;
+        return Math.floor(Math.random() * (maxY + 1));
     });
 
     return {
@@ -25,22 +21,14 @@ const chartOptions = {
     xAxis: {
         categories: ['Jan', 'Feb', 'Mar']
     },
-    yAxis: {
-        max: highestY,
-        softMax: highestY * 2,
-        plotLines: [{
-            color: 'green',
-            dashStyle: 'Dash',
-            value: highestY * 1.5,
-            width: 3
-        }]
-    },
     plotOptions: {
         series: {
             dataLabels: {
                 enabled: true,
                 formatter: function () {
-                    if (this.y === highestY) {
+                    const { dataMax } = this.series.chart.yAxis[0];
+
+                    if (this.y === dataMax) {
                         return 'max';
                     }
                 }
@@ -50,4 +38,18 @@ const chartOptions = {
     series
 }
 
-Highcharts.chart('container', chartOptions);
+const chart = Highcharts.chart('container', chartOptions);
+const highestY = chart.yAxis[0].dataMax;
+
+chart.update({
+    yAxis: {
+        max: highestY,
+        softMax: highestY * 2,
+        plotLines: [{
+            color: 'green',
+            dashStyle: 'Dash',
+            width: 3,
+            value: highestY * 1.5,
+        }]
+    },
+});
