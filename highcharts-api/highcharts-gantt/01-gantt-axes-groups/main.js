8f3fd880978e8c99303aed9a062bf633164e82f7
dqmrf <mykhailopelykh@tuta.io>
2026-06-23 10:18:53 +0200

    G01: Create gantt chart with axes groups

--- highcharts-api/highcharts-gantt/01-gantt-axes-groups/main.js
+++ highcharts-api/highcharts-gantt/01-gantt-axes-groups/main.js
@@ -1,23 +1,38 @@
+const borderColor = 'black';
+
 Highcharts.ganttChart('container', {
-
-  series: [{
-    name: 'Project 1',
-    data: [{
-      name: 'Main',
-      pointWidth: 0
-    }, {
-      name: 'First',
-      start: 1560902400000,
-      end: 1561075200000,
-    }, {
-      name: 'Second',
-      start: 1560902400000,
-      end: 1561075200000
-    }, {
-      name: 'Second',
-      start: 1561507200000,
-      end: 1561680000000
+    xAxis: [{
+        units: [['day']],
+        min: '2019-06-19 00:00',
+        max: '2019-06-24 23:59',
+        breaks: [{
+            from: '2019-06-19 23:59',
+            to: '2019-06-24 00:00',
+            breakSize: 0
+        }],
+        grid: { borderColor }
+    }],
+    yAxis: {
+        uniqueNames: true,
+        grid: { borderColor }
+    },
+    series: [{
+        name: 'Project 1',
+        data: [{
+            name: 'Main',
+            pointWidth: 0
+        }, {
+            name: 'First',
+            start: '2019-06-19 00:00',
+            end: '2019-06-19 11:00',
+        }, {
+            name: 'Second',
+            start: '2019-06-19 00:00',
+            end: '2019-06-19 11:00',
+        }, {
+            name: 'Second',
+            start: '2019-06-24 13:00',
+            end: '2019-06-24 23:59',
+        }]
     }]
-  }]
-
 });
