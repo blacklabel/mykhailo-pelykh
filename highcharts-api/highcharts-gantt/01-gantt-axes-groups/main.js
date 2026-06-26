f1b12cd9376baf2106123da94e02a3d0fcaa7e7d
dqmrf <mykhailopelykh@tuta.io>
2026-06-26 15:12:56 +0200

    G01: Minor refactoring

--- highcharts-api/highcharts-gantt/01-gantt-axes-groups/main.js
+++ highcharts-api/highcharts-gantt/01-gantt-axes-groups/main.js
@@ -1,5 +1,3 @@
-const borderColor = 'black';
-
 Highcharts.ganttChart('container', {
     xAxis: [{
         units: [['day']],
@@ -7,20 +5,18 @@ Highcharts.ganttChart('container', {
         max: '2019-06-24 23:59',
         breaks: [{
             from: '2019-06-19 23:59',
-            to: '2019-06-24 00:00',
-            breakSize: 0
+            to: '2019-06-24 00:00'
         }],
-        grid: { borderColor }
+        grid: { borderColor: 'black' }
     }],
     yAxis: {
         uniqueNames: true,
-        grid: { borderColor }
+        grid: { borderColor: 'black' }
     },
     series: [{
         name: 'Project 1',
         data: [{
-            name: 'Main',
-            pointWidth: 0
+            name: 'Main'
         }, {
             name: 'First',
             start: '2019-06-19 00:00',
